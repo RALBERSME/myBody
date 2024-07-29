8f214c780c81794515d952a8981b10bf69bc725d
RALBERSME <149826808+RALBERSME@users.noreply.github.com>
2024-07-29 16:53:29 +0200

    upload body6

---
+++ grain.js
@@ -0,0 +1,88 @@
+function showInfo1() {
+  document.getElementById("infoText").innerHTML = `
+    <h1>Einkorn</h1>
+        <p>
+          It is the oldest wheat and it is considered to be man’s first wheat.
+          It's name is derived from the German language and means “single
+          grain”.<br />
+          Because it has a very fragile structure, it is optimal for human
+          digestion.
+        </p>
+        <br />
+        <a
+          href="#"
+          onclick='window.open("https://www.einkorn.com/einkorn-history/"); return false;'
+          >read more on Einkorn
+        </a>
+        <br />
+    `;
+}
+function showInfo2() {
+  document.getElementById("infoText").innerHTML = `
+    <h1>Emmer</h1>
+        <p>
+          Emmer wheat or hulled wheat is a type of awned wheat.<br />
+          It is not as fragile as Einkorn, but it still has a moderate stability and therefore it is still ok for human digestion.
+        </p>
+        <br />
+        <a
+          href="#"
+          onclick='window.open("https://www.ancientgrains.com/emmer/emmer-history-and-origin/"); return false;'
+          >read more on Emmer
+        </a>
+        <br />
+    `;
+}
+function showInfo3() {
+  document.getElementById("infoText").innerHTML = `
+    <h1>Sourdough bread</h1>
+        <p>
+          Sourdough or sourdough bread is a bread made by the fermentation of dough using wild lactobacillaceae and yeast.<br />
+          Its structure is less complex but still more stable then the one of Einkorn. It is still better for human digestion than white wheat.
+        </p>
+        <br />
+        <a
+          href="#"
+          onclick='window.open("https://www.bbcgoodfood.com/howto/guide/what-sourdough"); return false;'
+          >read more on Sourdough
+        </a>
+        <br />
+    `;
+}
+function showInfo4() {
+  document.getElementById("infoText").innerHTML = `
+    <h1>Spelt</h1>
+        <p>
+          Spelt is an ancient member of the wheat family and an evolutionary hybrid of emmer wheat and goat grass. <br />
+          Its structure is less complex but still more stable then the one of Einkorn. It is still better for human digestion than white wheat.
+        </p>
+        <br />
+        <a
+          href="#"
+          onclick='window.open("https://www.healthline.com/nutrition/what-is-spelt"); return false;'
+          >read more on Spelt
+        </a>
+        <br />
+    `;
+}
+function showInfo5() {
+  document.getElementById("infoText").innerHTML = `
+    <h1>Modern Wheat</h1>
+        <p>
+          Breeding changed the growth of cereal plants. The stalks of the cereal were no longer as high and the cereal grains themselves became more stable. <br />
+        The new wheat leads to human digestion problems.
+        </p>
+        <br />
+         <a
+          href="#"
+          onclick='window.open("https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7756780/"); return false;'
+          >read more on modern Wheat from NIH (National Library of Medicine / USA)
+        </a>
+        <a
+          href="#"
+          onclick='window.open("https://www.reddit.com/r/AskHistorians/comments/lk7tt4/wheat_when_and_how_did_it_change_and_is_it_lost/"); return false;'
+          >read more on modern Wheat - this is a discussion on reddit
+        </a>
+        <br />
+    `;
+}
